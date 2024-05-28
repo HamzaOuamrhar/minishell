86ab3ba06d663617d564aa411f39a3febc965a34
HamzaOuamrhar <hamzaouamrhar09@gmail.com>
2024-05-28 19:29:39 +0100

    fixing norminette for quotes expanding

--- parsing/expander2.c
+++ parsing/expander2.c
@@ -46,7 +46,7 @@ void	inside_single_quote(t_decl2 *decl, t_token **token, char *t_v)
 	}
 }
 
-void	out_quotes(t_decl2 *decl, char *t_v, t_token **token)
+void	out_quotes_not_key(t_decl2 *decl, char *t_v, t_token **token)
 {
 	decl->start = decl->i;
 	while (t_v[decl->i] && t_v[decl->i] != '\'' && t_v[decl->i] != '"' && t_v[decl->i] != '$')
@@ -60,6 +60,73 @@ void	out_quotes(t_decl2 *decl, char *t_v, t_token **token)
 		else
 			(*token)->value = ft_strjoin((*token)->value, ft_substr(t_v, decl->start, decl->i - decl->start));
 	}
+}
+
+void	double_quote_key(t_decl2 *decl, char *t_v)
+{
+	decl->i += 1;
+	decl->start = decl->i;
+	while (t_v[decl->i] && (is_alph_num(t_v[decl->i]) || t_v[decl->i] == '_'))
+		decl->i++;
+	decl->value = getenv(ft_substr(t_v, decl->start, decl->i - decl->start));
+	if (decl->value)
+		decl->n_t_v = ft_strjoin(decl->n_t_v, decl->value);
+	else
+		decl->n_t_v = ft_strjoin(decl->n_t_v, "");
+}
+
+void	out_quotes(t_decl2 *decl, char *t_v, t_token **token)
+{
+	decl->start = decl->i;
+	if (t_v[decl->i] == '$')
+	{
+		decl->z = 0;
+		decl->i += 1;
+		decl->start = decl->i;
+		if (t_v[decl->i] && (!is_alph(t_v[decl->i]) || t_v[decl->i] != '_'))
+			decl->i++;
+		while (t_v[decl->i] && (is_alph_num(t_v[decl->i]) || t_v[decl->i] == '_'))
+			decl->i++;
+		decl->value = getenv(ft_substr(t_v, decl->start, decl->i - decl->start));
+		if (decl->value)
+		{
+			if (!is_white(decl->value[0]))
+			{
+				while (decl->value[decl->z] && !is_white(decl->value[decl->z]))
+					decl->z++;
+				if (!(*token)->flag && !decl->still)
+				{
+					decl->n_t_v = ft_strjoin(decl->n_t_v, ft_substr(decl->value, 0, decl->z));
+				}
+				else
+				{
+					if (decl->still)
+						add_middle_n(token, ft_substr(decl->value, 0, decl->z));
+					else
+						(*token)->value = ft_strjoin((*token)->value, ft_substr(decl->value, 0, decl->z));
+				}
+				if (decl->value[decl->z] && no_rest(decl->value, decl->z))
+					decl->still = 1;
+				else
+					decl->still = 0;
+			}
+			if (white_word(decl->value)){
+				add_middle_n(token, ft_strdup(decl->value));
+			}
+			else
+			{
+				add_middle(token, ft_split(decl->value + decl->z, ' ', &decl->still));
+			}
+			if (!(*token)->flag && word_count(decl->value) > 1)
+				(*token)->flag = 1;
+		}
+		else
+		{
+			decl->n_t_v = ft_strjoin(decl->n_t_v, "");
+		}
+	}
+	else
+		out_quotes_not_key(decl, t_v, token);
 }
 
 void	quotes_expander(t_token **token, char *t_v)
@@ -83,17 +150,7 @@ void	quotes_expander(t_token **token, char *t_v)
 			else
 			{
 				if (t_v[decl.i] == '$')
-				{
-					decl.i += 1;
-					decl.start = decl.i;
-					while (t_v[decl.i] && (is_alph_num(t_v[decl.i]) || t_v[decl.i] == '_'))
-						decl.i++;
-					decl.value = getenv(ft_substr(t_v, decl.start, decl.i - decl.start));
-					if (decl.value)
-						decl.n_t_v = ft_strjoin(decl.n_t_v, decl.value);
-					else
-						decl.n_t_v = ft_strjoin(decl.n_t_v, "");
-				}
+					double_quote_key(&decl, t_v);
 				else
 				{
 					decl.start = decl.i;
@@ -112,58 +169,7 @@ void	quotes_expander(t_token **token, char *t_v)
 			}
 		}
 		else
-		{
-			decl.start = decl.i;
-			if (t_v[decl.i] == '$')
-			{
-				decl.z = 0;
-				decl.i += 1;
-				decl.start = decl.i;
-				if (t_v[decl.i] && (!is_alph(t_v[decl.i]) || t_v[decl.i] != '_'))
-					decl.i++;
-				while (t_v[decl.i] && (is_alph_num(t_v[decl.i]) || t_v[decl.i] == '_'))
-					decl.i++;
-				decl.value = getenv(ft_substr(t_v, decl.start, decl.i - decl.start));
-				if (decl.value)
-				{
-					if (!is_white(decl.value[0]))
-					{
-						while (decl.value[decl.z] && !is_white(decl.value[decl.z]))
-							decl.z++;
-						if (!(*token)->flag && !decl.still)
-						{
-							decl.n_t_v = ft_strjoin(decl.n_t_v, ft_substr(decl.value, 0, decl.z));
-						}
-						else
-						{
-							if (decl.still)
-								add_middle_n(token, ft_substr(decl.value, 0, decl.z));
-							else
-								(*token)->value = ft_strjoin((*token)->value, ft_substr(decl.value, 0, decl.z));
-						}
-						if (decl.value[decl.z] && no_rest(decl.value, decl.z))
-							decl.still = 1;
-						else
-							decl.still = 0;
-					}
-					if (white_word(decl.value)){
-						add_middle_n(token, ft_strdup(decl.value));
-					}
-					else
-					{
-						add_middle(token, ft_split(decl.value + decl.z, ' ', &decl.still));
-					}
-					if (!(*token)->flag && word_count(decl.value) > 1)
-						(*token)->flag = 1;
-				}
-				else
-				{
-					decl.n_t_v = ft_strjoin(decl.n_t_v, "");
-				}
-			}
-			else
-				out_quotes(&decl, t_v, token);
-		}
+			out_quotes(&decl, t_v, token);
 	}
 	tmp->value = decl.n_t_v;
 }
