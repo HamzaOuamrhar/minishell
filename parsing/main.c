0270c6493d0a8d8cd0cf7f80a80590c600db9e93
HamzaOuamrhar <hamzaouamrhar09@gmail.com>
2024-05-29 15:19:49 +0100

    1337

--- parsing/main.c
+++ parsing/main.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv, char **env)
 	t_token	*token;
 	t_env	*env_vars;
 	t_parse	*parse;
-	// int		i;
+	int		i;
 
 	(void)argc;
 	(void)argv;
@@ -23,57 +23,52 @@ int main(int argc, char **argv, char **env)
 		tokenize(&token, line);
 		syntax(token);
 		expander(token);
-		while (token)
+		parser(token, &parse);
+		while (parse)
 		{
-			printf("%s\n",  token->value);
-			token = token->next;
+			i = 0;
+			puts("-------cmd------");
+			while (parse->cmd[i])
+			{
+				printf("%s\n", parse->cmd[i]);
+				i++;
+			}
+			puts("\n");
+			i = 0;
+			puts("-------in-------\n");
+			while (parse->in[i])
+			{
+				printf("%s\n", parse->in[i]);
+				i++;
+			}
+			puts("\n");
+			i = 0;
+			puts("-------out-------\n");
+			while (parse->out[i])
+			{
+				printf("%s\n", parse->out[i]);
+				i++;
+			}
+			puts("\n");
+			i = 0;
+			puts("--------app-------\n");
+			while (parse->app[i])
+			{
+				printf("%s\n", parse->app[i]);
+				i++;
+			}
+			puts("\n");
+			puts("---------in_dup------");
+			if (parse->in_dup)
+				printf("%s\n", parse->in_dup);
+			puts("\n");
+			puts("---------out_dup------");
+			if (parse->out_dup)
+				printf("%s\n", parse->out_dup);
+			parse = parse->next;
+			if (parse)
+			puts("**********next********");
 		}
-		// parser(token, &parse);
-		// while (parse)
-		// {
-		// 	i = 0;
-		// 	// puts("-------cmd------");
-		// 	// while (parse->cmd[i])
-		// 	// {
-		// 	// 	printf("%s\n", parse->cmd[i]);
-		// 	// 	i++;
-		// 	// }
-		// 	// puts("\n");
-		// 	// i = 0;
-		// 	// puts("-------in-------\n");
-		// 	// while (parse->in[i])
-		// 	// {
-		// 	// 	printf("%s\n", parse->in[i]);
-		// 	// 	i++;
-		// 	// }
-		// 	// puts("\n");
-		// 	// i = 0;
-		// 	// puts("-------out-------\n");
-		// 	// while (parse->out[i])
-		// 	// {
-		// 	// 	printf("%s\n", parse->out[i]);
-		// 	// 	i++;
-		// 	// }
-		// 	// puts("\n");
-		// 	// i = 0;
-		// 	// puts("--------app-------\n");
-		// 	// while (parse->app[i])
-		// 	// {
-		// 	// 	printf("%s\n", parse->app[i]);
-		// 	// 	i++;
-		// 	// }
-		// 	// puts("\n");
-		// 	// puts("---------in_dup------");
-		// 	// if (parse->in_dup)
-		// 	// 	printf("%s\n", parse->in_dup);
-		// 	// puts("\n");
-		// 	// puts("---------out_dup------");
-		// 	// if (parse->out_dup)
-		// 	// 	printf("%s\n", parse->out_dup);
-		// 	parse = parse->next;
-		// 	if (parse)
-		// 	puts("**********next********");
-		// }
 		tokens_reset(&token);
 	}
 }
