28f31ef3c95faee71ffadc2c1b7f16c09cee1a15
HamzaOuamrhar <hamzaouamrhar09@gmail.com>
2024-07-13 16:29:35 +0100

    expanding the 0 inside the heredoc

--- parsing/heredoc_expanding.c
+++ parsing/heredoc_expanding.c
@@ -15,11 +15,14 @@ void	expand_line(char **line, t_params *params)
 		if ((*line)[decl.i])
 			decl.i++;
 		decl.start = decl.i;
-		if (!is_alph((*line)[decl.i]) || (*line)[decl.i] != '_')
+		if (!is_alph((*line)[decl.i]) || (*line)[decl.i] != '_' || (*line)[decl.i] == '?')
 			decl.i++;
 		while ((*line)[decl.i] && (is_alph_num((*line)[decl.i]) || (*line)[decl.i] == '_'))
 			decl.i++;
-		decl.value = get_key(ft_mysubstr(*line, decl.start, decl.i - decl.start), params->env);
+		if ((*line)[decl.start] == '?')
+			decl.value = ft_strdup(ft_itoa(params->status));
+		else
+			decl.value = get_key(ft_mysubstr(*line, decl.start, decl.i - decl.start), params->env);
 		if (decl.value)
 			decl.n_t_v = ft_strjoin(decl.n_t_v, decl.value);
 		else
