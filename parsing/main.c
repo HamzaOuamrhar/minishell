d197485577f7dcd6a5be43b933bfd53441f5710c
HamzaOuamrhar <hamzaouamrhar09@gmail.com>
2024-07-20 20:59:42 +0100

    42

--- parsing/main.c
+++ parsing/main.c
@@ -27,10 +27,6 @@ void	wait_prompt1(t_params *params)
 	//remember the last word on the "_" env
 	initialiaze_vars(params, &i, &token, 1);
 	st = NULL;
-<<<<<<< HEAD
-=======
-	token = NULL;
->>>>>>> 6902ac367ac961c6e5b0f9116a71f2ae239d4fd1
 	while (1)
 	{
 		params->q = 0;
@@ -45,78 +41,77 @@ void	wait_prompt1(t_params *params)
 			{
 				expander(token, *params);
 				parser(token, &st, params);
-				print(st);
-				// params->cmds = lstsize(st);
-				// while (st)
-				// {
-				// 	update_(st, params);
-				// 	forking_checker(st, params, i);
-				// 	if ((!(params->pid) || (params->cmds == 1 && params->pid )) && checking_cmd(st, params))
-				// 	{
-				// 		tokens_reset(&token);
-				// 		parser_reset(&st);
-				// 		if (!params->pid)
-				// 			exit (0);
-				// 		else
-				// 			continue ;
-				// 	}
-				// 	slash_path(st, params);
-				// 	if (!params->path)
-				// 	{
-				// 		printf("Shellantics: %s: No such file or directory\n", st->cmd[0]);
-				// 		tokens_reset(&token);
-				// 		parser_reset(&st);
-				// 		if (!params->pid)
-				// 			exit (0);
-				// 		else
-				// 			continue ;
-				// 	}
-				// 	if (((!(params->pid)) || (params->cmds == 1 && params->pid )) && checking_cmd2(st, params))
-				// 	{
-				// 		tokens_reset(&token);
-				// 		parser_reset(&st);
-				// 		if (!params->pid)
-				// 			exit (0);
-				// 		else
-				// 			continue ;
-				// 	}
-				// 	if (!st->com_path || !ft_strlen(st->cmd[0]))
-				// 	{
-				// 		if (!params->pid)
-				// 		{
-				// 			close (params->fds[0]);
-				// 			close (params->fds[1]);
-				// 			exit (1);
-				// 		}
-				// 		params->flag = 1;
-				// 		printf("shellantics: %s :command not found\n", st->cmd[0]);
-				// 	}
-				// 	else
-				// 		excute_cmd(st, params);
-				// 	if (params->pid != 0)
-				// 	{
-				// 		if (i == params->cmds - 1)
-				// 		{
-				// 			waitpid(params->pid, 0, 0);
-				// 			// close(fds[0]);
-				// 			// close(fds[1]);
-				// 		}
-				// 		params->flag = 0; // this could cause a problem
-				// 		if (i != 0)
-				// 			close(params->save_fd);
-   				// 		if (i != params->cmds - 1)
-   				// 		{
-   				// 	   		close(params->fds[1]);
-				// 			params->save_fd = params->fds[0];
-				// 			params->flag_2 = 1;
-   				// 		}
-				// 	}
-				// 	st = st->next;
-				// 	i++;
-				// }
-				// initialiaze_vars(params, &i, &token, 0);
-				// while (wait(0) != -1 || errno != ECHILD)
-				// 	;
+				params->cmds = lstsize(st);
+				while (st)
+				{
+					update_(st, params);
+					forking_checker(st, params, i);
+					if ((!(params->pid) || (params->cmds == 1 && params->pid )) && checking_cmd(st, params))
+					{
+						tokens_reset(&token);
+						parser_reset(&st);
+						if (!params->pid)
+							exit (0);
+						else
+							continue ;
+					}
+					slash_path(st, params);
+					if (!params->path)
+					{
+						printf("Shellantics: %s: No such file or directory\n", st->cmd[0]);
+						tokens_reset(&token);
+						parser_reset(&st);
+						if (!params->pid)
+							exit (0);
+						else
+							continue ;
+					}
+					if (((!(params->pid)) || (params->cmds == 1 && params->pid )) && checking_cmd2(st, params))
+					{
+						tokens_reset(&token);
+						parser_reset(&st);
+						if (!params->pid)
+							exit (0);
+						else
+							continue ;
+					}
+					if (!st->com_path || !ft_strlen(st->cmd[0]))
+					{
+						if (!params->pid)
+						{
+							close (params->fds[0]);
+							close (params->fds[1]);
+							exit (1);
+						}
+						params->flag = 1;
+						printf("shellantics: %s :command not found\n", st->cmd[0]);
+					}
+					else
+						excute_cmd(st, params);
+					if (params->pid != 0)
+					{
+						if (i == params->cmds - 1)
+						{
+							waitpid(params->pid, 0, 0);
+							// close(fds[0]);
+							// close(fds[1]);
+						}
+						params->flag = 0; // this could cause a problem
+						if (i != 0)
+							close(params->save_fd);
+   						if (i != params->cmds - 1)
+   						{
+   					   		close(params->fds[1]);
+							params->save_fd = params->fds[0];
+							params->flag_2 = 1;
+   						}
+					}
+					st = st->next;
+					i++;
+				}
+				initialiaze_vars(params, &i, &token, 0);
+				while (wait(0) != -1 || errno != ECHILD)
+					;
 			}
 		}
 		tokens_reset(&token);
